058afb19bce0c58ad094fb80d995aaa2a2eaf5c4
Steven Kohlmeyer <Fastmover@2308284.no-reply.drupal.org>
2013-04-24 13:59:12 -0500

    Initial Commit

---
+++ PyXLS2PO.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# File to create:
+fileA = '/path/to/french.po'
+
+# XLS file to access - expecting columns: comment | english | other translation
+fileB = '/path/to/french_trans.xls'
+
+import xlrd
+import codecs
+import os
+import sys
+
+reload(sys).setdefaultencoding("utf8")
+
+print sys.getfilesystemencoding()
+print ""
+print sys.getdefaultencoding()
+print ""
+
+# Remove file, as to not append to.
+os.remove(fileA)
+
+f = codecs.open(fileA, mode='w', encoding='utf-8')
+
+book = xlrd.open_workbook(fileB, encoding_override="utf-8")
+worksheet = book.sheet_by_index(0)
+num_rows = worksheet.nrows - 1
+num_cells = worksheet.ncols - 1
+curr_row = -1
+while curr_row < num_rows:
+  curr_row += 1
+  row = worksheet.row(curr_row)
+  line1 = unicode(row[0].value)
+  line2 = unicode(row[1].value)
+  line3 = unicode(row[2].value)
+
+  # See output of the translated language, just for fun.
+  print line3
+
+  f.write(u"#: ")
+  f.write(line1.encode('utf8'))
+  f.write(u"\n")
+  f.write(u"msgid \"")
+  f.write(line2.encode('utf8'))
+  f.write(u"\"\n")
+  f.write(u"msgstr \"")
+  f.write(line3.decode('utf8'))
+  f.write(u"\"\n\n")
+
+f.close()
